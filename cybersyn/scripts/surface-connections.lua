741ed930167bb5af3e5717f120ea8f849ef45326
Harag <harag@cortexx.net>
2025-04-06 19:32:08 +0200

    Surface.status

--- cybersyn/scripts/surface-connections.lua
+++ cybersyn/scripts/surface-connections.lua
@@ -1,4 +1,12 @@
-local Surfaces = {}
+local Surfaces = {
+    ---@enum Cybersyn.SurfaceConnectionStatus
+    status = {
+        invalid = 0,
+        created = 1,
+        updated = 2,
+        deleted = 3,
+    }
+}
 
 local btest = bit32.btest
 local format = string.format
@@ -48,10 +56,10 @@ end
 -- removes the surface connection between the given entities from storage.SurfaceConnections. Does nothing if the connection doesn't exist.
 ---@param entity1 LuaEntity
 ---@param entity2 LuaEntity
----@return boolean
+---@return Cybersyn.SurfaceConnectionStatus
 function Surfaces.disconnect_surfaces(entity1, entity2)
     if not (entity1 and entity1.valid and entity2 and entity2.valid) then
-        return false -- these will eventually clean up in find_surface_connections()
+        return Surfaces.status.invalid -- these will eventually clean up in find_surface_connections()
     end
 
     local surface_pair_key = sorted_pair(entity1.surface.index, entity2.surface.index)
@@ -62,17 +70,17 @@ function Surfaces.disconnect_surfaces(entity1, entity2)
         if debug_log then log(format("removing surface connection for entities [%s] between surfaces [%s]", entity_pair_key, surface_pair_key)) end
         surface_connections[entity_pair_key] = nil
     end
-    return true
+    return Surfaces.status.deleted
 end
 
   -- adds a surface connection between the given entities; the network_id will be used in delivery processing to discard providers that don't match the surface connection's network_id
   ---@param entity1 LuaEntity
   ---@param entity2 LuaEntity
   ---@param network_masks {[string]: integer}?
-  ---@return boolean
+  ---@return Cybersyn.SurfaceConnectionStatus
   function Surfaces.connect_surfaces(entity1, entity2, network_masks)
     if not (entity1 and entity1.valid and entity2 and entity2.valid) then
-        return false
+        return Surfaces.status.invalid
     end
 
     if entity1.surface == entity2.surface then
@@ -81,27 +89,30 @@ end
             entity1.unit_number, entity2.unit_number,
             entity1.surface.name, entity1.surface.index))
         end
-        return false
+        return Surfaces.status.invalid
     end
 
     local surface_pair_key = sorted_pair(entity1.surface.index, entity2.surface.index)
     local surface_connections = get_or_create(storage.connected_surfaces, surface_pair_key)
 
     local entity_pair_key = sorted_pair(entity1.unit_number, entity2.unit_number)
+    local is_update = surface_connections[entity_pair_key] and true
+
     if debug_log then
-        log(format("(connect_surfaces) Creating surface connection between [%d] on %s [%d] and [%d] on %s [%d].",
+        log(format("(connect_surfaces) %s surface connection between [%d] on %s [%d] and [%d] on %s [%d].",
+        is_update and "Updating" or "Creating",
         entity1.unit_number, entity1.surface.name, entity1.surface.index,
         entity2.unit_number, entity2.surface.name, entity2.surface.index))
     end
 
     -- enforce a consistent order for repeated calls with the same two entities
     if entity2.unit_number < entity1.unit_number then
-        surface_connections[entity_pair_key] = { entity1 = entity2, entity2 = entity1, network_id = network_masks }
+        surface_connections[entity_pair_key] = { entity1 = entity2, entity2 = entity1, network_masks = network_masks }
     else
-        surface_connections[entity_pair_key] = { entity1 = entity1, entity2 = entity2, network_id = network_masks }
+        surface_connections[entity_pair_key] = { entity1 = entity1, entity2 = entity2, network_masks = network_masks }
     end
 
-    return true
+    return is_update and Surfaces.status.updated or Surfaces.status.created
 end
 
 function Surfaces.on_surface_deleted(event)
